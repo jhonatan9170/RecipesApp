77bccf2673f9b02ef1c740292fdc7fc284e533e7
Jhonatanm <65328388+jhonatan9170@users.noreply.github.com>
2023-11-10 23:52:49 -0500

    flow LocationVC completed

--- RecipesApp/Views/LocationViewController.swift
+++ RecipesApp/Views/LocationViewController.swift
@@ -12,18 +12,46 @@ class LocationViewController: UIViewController {
     
     @IBOutlet weak var locationView:MKMapView!
     
+    var viewModel: LocationViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Ica, Perú"
-        let centre = CLLocationCoordinate2D(latitude:  -9.189967, longitude: -75.015152)
-        locationView.setCenter(centre, animated: false)
-        let pin = MKPointAnnotation()
-        pin.coordinate = centre
-        locationView.addAnnotation(pin)
-        navigationController?.navigationBar.tintColor = .white
-        navigationController?.navigationBar.backItem?.title = "Anything Else"
+
+        setupViews()
+        viewModel.delegate = self
+        viewModel.getCoordinates()
     }
     
+    func setupViews() {
+        view.addSpinner()
+        self.title = viewModel.location + ", Perú"
+        let centre = CLLocationCoordinate2D(latitude:  -9.189967, longitude: -75.015152)
+        locationView.setCenter(centre, animated: false)
+        locationView.isHidden = true
+    }
+}
 
+extension LocationViewController: LocationViewModelDelegate{
+    func success(_ viewModel: LocationViewModel) {
+        guard let coordinates = viewModel.coordinates else {
+            error(viewModel)
+            return
+        }
+        locationView.isHidden = false
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinates
+        DispatchQueue.main.async { [weak self] in
+            self?.view.removeSpinner()
+            self?.locationView.addAnnotation(pin)
+        }
+    }
+    
+    func error(_ viewModel: LocationViewModel) {
+
+        DispatchQueue.main.async { [weak self] in
+            self?.view.removeSpinner()
+            self?.locationView.isHidden = false
+        }
+    }
 
 }
